0d5526644750d4c3df99499aa6764ffb41a867b5
NguyenLamSon-123 <k225480106076@tnut.edu.vn>
2026-03-24 22:32:32 +0700

    Delete BAITAPLON directory

--- BAITAPLON/src/classify.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def classify_leaf(img):
-    # Tính trung bình màu để phân biệt lá khỏe hay bệnh
-    mean_color = np.mean(img, axis=(0,1))
-    if mean_color[1] < 80:  # kênh xanh thấp -> lá úa/bệnh
-        return "Bệnh: Vàng lá"
-    else:
-        return "Lá khỏe mạnh"
